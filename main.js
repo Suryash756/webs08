efe11d9c6cfff63ed70b9b8165b2e69f62bfbc75
Suryash756 <86527278+Suryash756@users.noreply.github.com>
2022-03-07 07:52:15 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+ //code
+ console.log("this is webs library.");
+ //concat
+ var name="Webs";
+ var name_last="Lbrary";
+ var name_full=name.concat(name_last);
+ console.log(name_full);
+ console.log("developer name Suryash jha");
+ var logo="webs library";
+ var logo_length=logo.length;
+ console.log(logo_length);
+ //maths
+ var x=4;
+ var y=25;
+ var product=4*25;
+ console.log(product);
+ var web= "web";
+ var search="search";
+ var result=web.concat(search);
+ console.log(result);
+ var google="google";
+ var webs="webs library";
+ var change=webs.replace(google);
+ console.log(change);
+ var capital="webs library"
+ var out=capital.toUpperCase();
+ console.log(out);
+ var small="WEBS LIBRARY";
+ var small_letter=small.toLowerCase();
+ console.log(small_letter);
+ var length_name= "webs library";
+ var real_length= length_name.length;
+ console.log(real_length);
+//search code
+
+var  search= document.getElementById("input_search").value;
+
+function search_thing(){
+
